4e0aa3e53db6b1a193fe78e451a71d009342f0f6
Omar Bsoul <omarbsoul@gmail.com>
2020-03-30 23:24:54 +0300

    adding generic typing for validators

--- src/core/validators/validator.ts
+++ src/core/validators/validator.ts
@@ -1,27 +1,27 @@
 import Joi from "@hapi/joi";
 
-export interface IValidator {
+export interface IValidator<T> {
   objectId(): Joi.Schema;
-  validateNew<T>(object: T): Promise<T>;
-  validateUpdate<T>(object: T): Promise<T>;
+  validateNew(object: T): Promise<T>;
+  validateUpdate(object: T): Promise<T>;
 }
 
-export abstract class ValidatorBase implements IValidator {
+export abstract class ValidatorBase<T> implements IValidator<T> {
   public objectId() {
     return Joi.string()
       .regex(/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i, { name: "object id" })
       .trim();
   }
 
-  public validateNew<T>(object: T) {
+  public validateNew(object: T) {
     return this.validate(this.getNewSchema(), object);
   }
 
-  public validateUpdate<T>(object: T) {
+  public validateUpdate(object: T) {
     return this.validate(this.getUpdateSchema(), object);
   }
 
-  private async validate<T>(schema: Joi.Schema, object: T): Promise<T> {
+  private async validate(schema: Joi.Schema, object: T): Promise<T> {
     try {
       return await schema.validateAsync(object, {
         abortEarly: false
