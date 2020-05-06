3b0822ea5788b528506c17163229e5a866db1c2a
Omar Bsoul <omarbsoul@gmail.com>
2020-05-06 03:39:37 +0300

    fixing 400 bug when validation error

--- src/core/controllers/error.controller.ts
+++ src/core/controllers/error.controller.ts
@@ -4,8 +4,9 @@ import { ApiError } from "../types";
 export const errorController = (error: ApiError | Error, req: Request, res: Response, next: NextFunction) => {
   if (error) {
     if (error instanceof ApiError) {
-      const isJoi = (error as any).isJoi;
-      res.status(isJoi ? 400 : error.code).send({ error: error.errorInfo });
+      res.status(error.code).send({ error: error.errorInfo });
+    } else if ((error as any).isJoi) {
+      res.status(400).send({ error: error.message });
     } else {
       res.status(500).send(process.env.NODE_ENV === "production" ? { error: "internal server error" } : error.message);
     }
